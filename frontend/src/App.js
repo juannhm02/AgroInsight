afa27ed060476c2b23d94f5a8b1cdea5ce406b2c
Juan Higuera <juanhmohedano123@gmail.com>
2024-11-18 21:04:04 +0100

    connecting database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    useEffect(() => {
+        axios.get("http://localhost:5000")
+            .then((response) => console.log(response.data))
+            .catch((error) => console.error(error));
+    }, []);
+
+    return <div>Welcome to AgroInsight</div>;
+};
 
 export default App;
